b2a8cd66f500ce2d0878dd0a062e0b8b459e2abe
MdMonirHossan <mdraju01820@gmail.com>
2026-07-13 19:25:31 +0600

    Working on webhooks and  tests

--- config/settings/local.py
+++ config/settings/local.py
@@ -70,5 +70,6 @@ CSRF_TRUSTED_ORIGINS = env.list(
         "http://127.0.0.1:8000",
         "http://127.0.0.1:8003",
         "http://localhost:8003",
+        "https://outline-kilometer-abroad.ngrok-free.dev",
     ],
 )
